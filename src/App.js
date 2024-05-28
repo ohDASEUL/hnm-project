c50ce8397a5493c164e41b18b9666f27ecafcd57
ohDASEUL <comeseul312@gmail.com>
2024-05-28 20:48:43 +0900

    라우터 세팅 및 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+/*
+1. 전체 상품 페이지, 로그인, 상품 상세 페이지(완료)
+2. 전체 상품 페이지 : 전체 상품 볼 수 있음
+3. 로그인 페이지 : 로그인 버튼 클릭 시
+4. 상품을 눌렀으나 로그인이 안 된 경우 로그인 페이지가 나오고
+      로그인이 된 경우 상품 상세 페이지가 나옴
+5. 로그아웃 버튼 클릭 시 로그아웃
+6. 로그아웃이 되면 상품 상세 페이지 볼 수 없고, 다시 로그인 해야 보임
+7. 로그인 하면 로그아웃 문구가 보이고, 로그아웃하면 로그인 문구가 보임
+8. 상품 검색 가능
+*/
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ProductAllPage from './pages/ProductAllPage';
+import LoginPage from './pages/LoginPage';
+import ProductDetailPage from './pages/ProductDetailPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <Routes>
+      <Route path="/" element={<ProductAllPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/product/:id" element={<ProductDetailPage/>}/>
+     </Routes>
     </div>
   );
 }
---
+++ src/pages/ProductDetailPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDetailPage = () => {
+  return (
+    <div>
+        <h1>상품 상세 페이지</h1>
+    </div>
+  )
+}
+
+export default ProductDetailPage
