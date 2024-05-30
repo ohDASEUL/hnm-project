3a85593c389b65995b3d4ed9d8d90f2486fbf857
ohDASEUL <comeseul312@gmail.com>
2024-05-30 21:14:31 +0900

    api 호출

---
+++ src/components/ProductCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src='https://lp2.hm.com/hmgoepprod?set=source[/3a/1b/3a1bfc28ea15ac12227f012c9f7d603c333840af.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]'/>
+        <div>Conscious choice</div>
+        <div>코튼 멕시 드레스</div>
+        <div>￦59,900</div>
+        <div>신제품</div>
+        
+
+    </div>
+  )
+}
+
+export default ProductCard
--- src/pages/ProductAllPage.js
+++ src/pages/ProductAllPage.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../components/ProductCard';
 
 const ProductAllPage = () => {
+  const [productList, setProductList] = useState([]);
+  const getProducts= async()=>{
+    let url = `http://localhost:5000/products`
+    let res = await fetch(url);
+    let data = await res.json()
+    setProductList(data)
+  }
+  useEffect(()=>{
+    getProducts()
+  },[])
   return (
     <div>
-        <h1>전체 상품 페이지</h1>
+        <ProductCard/>
     </div>
   )
 }
