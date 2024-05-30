ce1fef1097913b56d0f6ba172a138cfcb1ef7b16
ohDASEUL <comeseul312@gmail.com>
2024-05-30 20:42:46 +0900

    navbar 코드 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
+import React from "react";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+
 const Navbar = () => {
   const menuList = [
     "Women",
@@ -14,38 +15,31 @@ const Navbar = () => {
     "지속가능성",
   ];
   return (
-    <div>
-      <div>
-        <div className="login-btn">
+    <div className="navbar-area">
+      <div className="logo-login-area">
+        <div className="logo-area">
+          <img
+            src="https://i.namu.wiki/i/ayYKA61pz1lLaG_PaT3AQQCtgd6fXn2SARPuSKp0zsllO7vWa2nK1BXZor6cvq1agjdwlntjGFqqLt85KnnatjJP3QvoYMsr9w_mM4eokzIqHW6KCy7xiXbt6Vi-j0pV4aHkUqcd0H71V-BojF04-w.svg"
+            width="61px"
+          />
+        </div>
+        <div className="login-area">
           <FontAwesomeIcon icon={faUser} />
-          <div>로그인</div>
+          <span>로그인</span>
         </div>
       </div>
-
-      <div className="nav-section">
-        <img
-          src="https://i.namu.wiki/i/ayYKA61pz1lLaG_PaT3AQQCtgd6fXn2SARPuSKp0zsllO7vWa2nK1BXZor6cvq1agjdwlntjGFqqLt85KnnatjJP3QvoYMsr9w_mM4eokzIqHW6KCy7xiXbt6Vi-j0pV4aHkUqcd0H71V-BojF04-w.svg"
-          width="61px"
-        />
-      </div>
-
-      <div class="menu-area">
-        <div class="flex-container">
-          <ul className="menu-list">
-            {menuList.map((menu) => (
-              <li>{menu}</li>
-            ))}
-          </ul>
-          <div class="menu-search-area">
-            <div className="menu-search">
-              <FontAwesomeIcon icon={faSearch} />
-              <input type="text" />
-            </div>
-          </div>
+      <div className="menu-serch-area">
+        <ul>
+          {menuList.map((menu) => (
+            <li key={menu}>{menu}</li>
+          ))}
+        </ul>
+        <div className="serch-group">
+          <FontAwesomeIcon icon={faSearch} />
+          <input type="text" />
         </div>
       </div>
     </div>
   );
 };
-
 export default Navbar;
