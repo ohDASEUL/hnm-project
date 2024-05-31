055bb22f6e580a87ffc37a384b058354f895f49d
ohDASEUL <comeseul312@gmail.com>
2024-05-31 10:55:35 +0900

    db.json에 있는 정보들 ProductCard에 불러오고, 리액트 부트스트랩으로 디자인 꾸며주기

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({item}) => {
   return (
     <div>
-        <img src='https://lp2.hm.com/hmgoepprod?set=source[/3a/1b/3a1bfc28ea15ac12227f012c9f7d603c333840af.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]'/>
-        <div>Conscious choice</div>
-        <div>코튼 멕시 드레스</div>
-        <div>￦59,900</div>
-        <div>신제품</div>
+        <img src={item?.img} width="300px"/>
+        <div>{item?.choice === true?"concious choice":""}</div>
+        <div>{item?.title}</div>
+        <div>{item?.price}</div>
+        <div>{item?.new === true?"신제품":""}</div>
         
 
     </div>
--- src/pages/ProductAllPage.js
+++ src/pages/ProductAllPage.js
@@ -1,22 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import ProductCard from '../components/ProductCard';
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 const ProductAllPage = () => {
   const [productList, setProductList] = useState([]);
-  const getProducts= async()=>{
-    let url = `http://localhost:5000/products`
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
     let res = await fetch(url);
-    let data = await res.json()
-    setProductList(data)
-  }
-  useEffect(()=>{
-    getProducts()
-  },[])
+    let data = await res.json();
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div>
-        <ProductCard/>
+      <Container>
+        <Row>
+          {productList.map((menu) => (
+            <Col lg={3}>
+              <ProductCard item={menu}/>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default ProductAllPage
+export default ProductAllPage;
