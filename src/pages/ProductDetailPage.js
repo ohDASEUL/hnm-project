1ffa833d36d14c3e4054ea904b501d4c7440551c
ohDASEUL <comeseul312@gmail.com>
2024-06-03 11:53:17 +0900

    상품 디테일 페이지 제작

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Button, Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
+  const [product, setProduct] = useState(null);
+  let { id } = useParams();
+  const getProductDetail = async () => {
+    let url = `http://localhost:5000/products/${id}`;
+    let res = await fetch(url);
+    let data = await res.json();
+    setProduct(data);
+  };
+  useEffect(() => {
+    getProductDetail();
+  }, []);
   return (
-    <div>
-        <h1>상품 상세 페이지</h1>
-    </div>
-  )
-}
+    <Container>
+      <div className="product-detail-group">
+        <img src={product?.img} width="300px" />
+        <div className="product-detail-info">
+          <div>{product?.title}</div>
+          <div>￦{product?.price}</div>
+          <div>{product?.choice === true ? "concious choice" : ""}</div>
+          <select style={{ width: "150px" }}>
+            <option value="" disabled selected>
+              사이즈 선택
+            </option>
+            <option value="1">S</option>
+            <option value="2">M</option>
+            <option value="3">L</option>
+          </select>
+          <Button variant="danger" style={{ width: "400px" }}>추가</Button>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-export default ProductDetailPage
+export default ProductDetailPage;
